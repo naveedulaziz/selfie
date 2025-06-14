ef62fdef27dee7937db54c0a8f9c7d720a6462c9
Naveed Ul Aziz <naveedulaziz@gmail.com>
2025-06-14 21:27:40 +0500

    Login from extras

--- Js/index.js
+++
@@ -1,7 +0,0 @@
-$('.toggle').on('click', function() {
-  $('.container1').stop().addClass('active');
-});
-
-$('.close').on('click', function() {
-  $('.container1').stop().removeClass('active');
-});
